acad68c1ac36553515ffb40d317e9e77da0413cd
Aucarure <victor.aucarure@tecsup.edu.pe>
2025-03-26 08:59:57 -0500

    Update ejercicio3.py

--- semana2/ejercicio3.py
+++ semana2/ejercicio3.py
@@ -1,1 +1,10 @@
-print("Hole")
+#Inveritr la cadena
+def invertir_cadena(cadena):
+    if len(cadena) == 0:  
+        return cadena
+    return cadena[-1] + invertir_cadena(cadena[:-1])  
+
+
+texto = "Hola Mundo"
+resultado = invertir_cadena(texto)
+print(resultado) 
